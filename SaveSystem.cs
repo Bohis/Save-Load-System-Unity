7596014f1666e78f3efca5e1ebdbddaf439f270a
Bohis <murashov.b@yandex.ru>
2021-06-06 18:37:44 +0500

    Adding comment

--- SaveSystem.cs
+++ SaveSystem.cs
@@ -5,10 +5,18 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 using UnityEngine;
 
+/// <summary>
+/// Система загрузки и сохранения данных
+/// </summary>
 public static class SaveSystem {
-    
+    /// <summary>
+    /// Cохрание класса в файл
+    /// </summary>
+    /// <typeparam name="T">Тип сохраняймого класса</typeparam>
+    /// <param name="data">объект класса T</param>
+    /// <param name="path">название файла, без расширения</param>
     public static void SaveData<T>(T data, string path) where T : class {
-        path = Application.persistentDataPath + "/" + path;
+        path = Application.persistentDataPath + "/" + path + ".bin";
 
         using (FileStream stream = new FileStream(path, FileMode.Create)) {
             BinaryFormatter formatter = new BinaryFormatter();
@@ -16,8 +24,15 @@ public static class SaveSystem {
         }
     }
 
+
+    /// <summary>
+    /// Загрузка класса из файла
+    /// </summary>
+    /// <typeparam name="T">Тип загружаймого класса</typeparam>
+    /// <param name="path">Название загружаймого файла</param>
+    /// <returns>Результат загрузки</returns>
     public static T LoadData<T>(string path) where T : class {
-        path = Application.persistentDataPath + "/" + path;
+        path = Application.persistentDataPath + "/" + path + ".bin";
 
         using (FileStream stream = new FileStream(path, FileMode.Open)) {
             BinaryFormatter formatter = new BinaryFormatter();
